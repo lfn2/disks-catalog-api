e22c18599c25b39c29db043943b3c98c58cc98ea
Lucas <lucasfnetto@gmail.com>
2018-11-30 15:19:43 -0300

    chore: configuring database

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var logger = require('morgan');
-
+var debug = require('debug')('disks-catalog-api:server');
+var db = require('./db/db');
+var models = require('./models');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var connectToDb = async function() {
+  try {
+    await db.authenticate();
+    await db.sync();
+    debug('Connected to database')
+  } catch (error) {
+    debug(error);
+    process.exit(1);
+  }
+}
+connectToDb();
 
 module.exports = app;
---
+++ db/db.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operatorsAliases: false
+});
+
+module.exports = sequelize;
