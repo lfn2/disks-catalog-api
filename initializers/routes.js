bbbd8c43f4b20676f153374e8cc8e67fd4399fb3
Lucas <lucasfnetto@gmail.com>
2018-12-01 12:51:31 -0300

    feat(routes): adding cors

--- initializers/routes.js
+++ initializers/routes.js
@@ -3,9 +3,11 @@ const logger = require('morgan');
 const error = require('../middlewares/error');
 const disks = require('../routes/disks');
 const collections = require('../routes/collections');
+const cors = require('cors');
 
 module.exports = function(app) {
   app.use(logger('dev'));
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use('/api/disks', disks);
